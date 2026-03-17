594b5c89ab514a3e411f1417fb78753fcb82b249
DornalaAdithya <dornalaadithya@gmail.com>
2026-03-18 00:13:57 +0530

    updated api url

--- FRONTEND/src/api/axios.js
+++ FRONTEND/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://ai-resume-analyzer-20gi.onrender.com/api",
   withCredentials: true, //  IMPORTANT for cookies
 });
 
